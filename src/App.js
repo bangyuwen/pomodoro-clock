e23916447501f2251183d156ee79fb36c1e38b99
BangYuWen <bangyuwen@gmail.com>
2017-09-09 16:36:17 +0800

    basic ui

--- src/App.js
+++ src/App.js
@@ -1,21 +1,56 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      sessionLength: 25,
+      breakLength: 5,
+      timer: [25, 0]
+    }
+    this.setSesstion = this.setSesstion.bind(this)
+    this.setBreak = this.setBreak.bind(this)
+  }
+  setSesstion(e) {
+    console.log(e.target.innerHTML)
+  }
+  setBreak(e) {
+    console.log(e.target.value)
+  }
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h2>Pomodoro Clock</h2>
+        <Display timer={this.state.timer}/>
+        <SessionSetting set={this.setSesstion}/>
+        <BreakSetting set={this.setBreak}/>
       </div>
-    );
+    )
   }
 }
+
+let Display = ({timer}) => (
+  <div>
+    <h1>{`${timer[0]} : ${timer[1]}`}</h1>
+  </div>
+)
+
+let SessionSetting = ({set}) => (
+  <div>
+    <h3>session length</h3>
+    <button onClick={set}>+</button>
+    <button onClick={set}>-</button>
+  </div>
+)
+
+let BreakSetting = ({set}) => (
+  <div>
+    <h3>break length</h3>
+    <button onClick={set}>+</button>
+    <button onClick={set}>-</button>
+  </div>
+)
+
 
 export default App;
