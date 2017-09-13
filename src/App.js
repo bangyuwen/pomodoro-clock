e3994d068142d3ffb81015b8d0a8d3839941e0dd
BangYuWen <bangyuwen@gmail.com>
2017-09-13 13:23:22 +0800

    function ok

--- src/App.js
+++ src/App.js
@@ -7,50 +7,121 @@ class App extends Component {
     this.state = {
       sessionLength: 25,
       breakLength: 5,
-      timer: [25, 0]
+      secLeft: 25 * 60,
+      isOn: false,
+      type: 'session'
     }
     this.setSesstion = this.setSesstion.bind(this)
     this.setBreak = this.setBreak.bind(this)
+    this.toggleTimer = this.toggleTimer.bind(this)
   }
   setSesstion(e) {
-    console.log(e.target.innerHTML)
+    let action = e.target.innerHTML
+    switch (action) {
+      case '+':
+        this.setState({
+          sessionLength: this.state.sessionLength + 1,
+          secLeft: (this.state.sessionLength + 1) * 60
+        })
+        break;
+      default:
+        if (this.state.sessionLength === 1) return
+        this.setState({
+          sessionLength: this.state.sessionLength - 1,
+          secLeft: (this.state.sessionLength - 1) * 60
+        })
+    }
   }
   setBreak(e) {
-    console.log(e.target.value)
+    let action = e.target.innerHTML
+    switch (action) {
+      case '+':
+        this.setState({
+          breakLength: this.state.breakLength + 1,
+          secLeft: (this.state.breakLength + 1) * 60
+        })
+        break;
+      default:
+        if (this.state.breakLength === 1) return
+        this.setState({
+          breakLength: this.state.breakLength - 1,
+          secLeft: (this.state.breakLength - 1) * 60
+        })
+    }
+  }
+  toggleTimer() {
+    console.log(this.state.isOn)
+    if (this.state.isOn) {
+      this.setState({isOn: false})
+      return clearInterval(this.timer)
+    }
+    this.setState({
+      isOn: true
+    })
+    this.timer = setInterval(()=> {
+      let secLeft = this.state.secLeft
+      if (secLeft === 0) {
+        this.setState({
+          secLeft: this.state.type === 'session' ?
+                   this.state.breakLength * 60 :
+                   this.state.sessionLength * 60,
+          type: this.state.type === 'session' ? 'break' : 'session'
+        })
+      } else {
+        this.setState({
+          secLeft: secLeft - 1
+        })
+      }
+    }, 1000)
   }
   render() {
     return (
       <div className="App">
         <h2>Pomodoro Clock</h2>
-        <Display timer={this.state.timer}/>
-        <SessionSetting set={this.setSesstion}/>
-        <BreakSetting set={this.setBreak}/>
+        <Countdown seconds={this.state.secLeft}
+                   toggle={this.toggleTimer}
+                   isOn={this.state.isOn}/>
+        <SessionSetting set={this.setSesstion}
+                        timeLength={this.state.sessionLength} />
+        <BreakSetting set={this.setBreak}
+                      timeLength={this.state.breakLength} />
       </div>
     )
   }
 }
 
-let Display = ({timer}) => (
-  <div>
-    <h1>{`${timer[0]} : ${timer[1]}`}</h1>
-  </div>
-)
+let Countdown = ({seconds, toggle, isOn}) => {
+  let parseSeconds = (sec) => {
+    let m = Math.floor(seconds / 60)
+    let s = sec % 60
+    s = s > 9 ? s : '0' + s
+    return(`${m}:${s}`)
+  }
+  return(
+    <div>
+      <h1>{parseSeconds(seconds)}</h1>
+      <button onClick={toggle}>{isOn ? 'Stop' : 'Start'}</button>
+    </div>
+  )
+}
 
-let SessionSetting = ({set}) => (
+
+let SessionSetting = ({set, timeLength}) => (
   <div>
     <h3>session length</h3>
     <button onClick={set}>+</button>
+    <span>{timeLength}</span>
     <button onClick={set}>-</button>
   </div>
 )
 
-let BreakSetting = ({set}) => (
+let BreakSetting = ({set, timeLength}) => (
   <div>
     <h3>break length</h3>
     <button onClick={set}>+</button>
+    <span>{timeLength}</span>
     <button onClick={set}>-</button>
   </div>
 )
-
 
 export default App;
